edc77c7f0760522cec0aa6938fd8be239ca8280d
RajaduraiAz <arajadurai1@live.com>
2024-11-02 21:10:58 +0530

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,8 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.rajaduraiaz.com',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	}),
+  site: 'https://blog.rajaduraiaz.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel()
 });
